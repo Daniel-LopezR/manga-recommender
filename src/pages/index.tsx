54e5980dd05d9398dceccc60ec918acb6beb8be0
Daniel <d.lopez4@sapalomera.cat>
2022-11-08 22:52:54 +0100

    Some UI improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,46 +30,48 @@ export default function Home() {
         </div>
       </div>
       <div className="p-4" />
-      <button className=" p-2 border rounded-2xl w-64 hover:bg-red-900 transition">
+      <button className="p-2 border rounded-2xl w-64 hover:bg-red-900 transition">
         Recommend me!
       </button>
       <div className="p-2" />
-      {dataLoaded && (
-        <div className="flex flex-col justify-center items-center">
-          <div className="text-2xl text-center p-4 w-128">
-            {data?.manga.alternative_titles.ja === ""
-              ? data?.manga.title
-              : data?.manga.title + " - " + data?.manga.alternative_titles.ja}
+      <div className="flex-1 w-80 h-full">
+        {dataLoaded && (
+          <div className="flex flex-col justify-center items-center h-full">
+            <div className="text-2xl text-center p-2">
+              {data?.manga.alternative_titles.ja === ""
+                ? data?.manga.title
+                : data?.manga.title + " - " + data?.manga.alternative_titles.ja}
+            </div>
+            {data?.manga.main_picture !== undefined ? (
+              <img
+                className="shadow-md shadow-white rounded-lg h-full w-full"
+                src={data?.manga.main_picture.large}
+                alt={data?.manga.title + " manga cover"}
+              />
+            ) : (
+              <img
+                className="p-6 invert h-full w-full"
+                src={"question-mark.svg"}
+                alt={data?.manga.title + " doesn't have manga cover"}
+              />
+            )}
+            <div className="p-2" />
+            <a
+              href=""
+              className="border rounded-xl bg-cyan-700 p-2 hover:bg-cyan-600 transition"
+            >
+              + Info
+            </a>
           </div>
-          {data?.manga.main_picture !== undefined ? (
-            <img
-              className="shadow-md shadow-white rounded-lg"
-              src={data?.manga.main_picture.large}
-              alt={data?.manga.title + " manga cover"}
-              width={256}
-            />
-          ) : (
-            <img
-              className="p-6 invert"
-              src={"question-mark.svg"}
-              alt={data?.manga.title + " doesn't have manga cover"}
-              width={256}
-            />
-          )}
-          <div className="p-2" />
-          <a
-            href=""
-            className="border rounded-xl bg-cyan-700 p-2 hover:bg-cyan-600 transition"
-          >
-            + Info
-          </a>
-        </div>
-      )}
-      {!dataLoaded && (
-        <div className="flex flex-col justify-center p-60">
-          <img src="ball-triangle.svg" />
-        </div>
-      )}
+        )}
+        {!dataLoaded && (
+          <div className="flex flex-col justify-center items-center h-full">
+            <img className="" src="ball-triangle.svg"/>
+          </div>
+        )}
+      </div>
+      <div className="p-3"></div>
+      <div className="text-1xl text-center p-4"><a href="https://github.com/Daniel-LopezR/manga-recommender" target="_blank">GitHub</a></div>
     </div>
   );
 }
