52779bb62e0d937c03d4f649a6d760896d04ab8a
Daniel <d.lopez4@sapalomera.cat>
2022-11-27 18:12:19 +0100

    Finally fixed top level await to always get correct amout of manga

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: ["api-cdn.myanimelist.net"] },
+  webpack: (config) => {
+    config.experiments = {
+        "topLevelAwait": true
+    }
+    return config
+  },
 };
 
 module.exports = nextConfig;
--- src/utils/getRandomManga.ts
+++ src/utils/getRandomManga.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/backend/utils/prisma";
 
-const MAX_MANGA_ID:number = 4000;
+const MAX_MANGA_ID:number = await prisma.manga.count();
 
 export const getRandomManga: (notThisOne?: number) => number = (
   notThisOne?: number
