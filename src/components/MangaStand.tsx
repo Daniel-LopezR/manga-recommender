ac57216db60a56d090c530440ea1ef3d6cf40804
Daniel <d.lopez4@sapalomera.cat>
2023-01-11 22:38:45 +0100

    Passed manga prop to const, now its more readable, changed conditional

--- src/components/MangaStand.tsx
+++ src/components/MangaStand.tsx
@@ -5,9 +5,11 @@ import { inferQueryResponse } from "../pages/api/trpc/[trpc]";
 type MangaFromServer = inferQueryResponse<"get-manga-by-id">;
 
 const MangaStand: React.FC<{ mangaFS: MangaFromServer }> = (props) => {
+  const manga = props.mangaFS.manga;
+
   return (
     <>
-      {props.mangaFS.manga === null ? (
+      {manga === null ? (
         <div className="flex flex-col justify-center items-center w-full">
           <div className="text-center">
             There are no manga that satisfies the requested options
@@ -16,33 +18,33 @@ const MangaStand: React.FC<{ mangaFS: MangaFromServer }> = (props) => {
       ) : (
         <div className="h-full flex flex-col items-center justify-center">
           <div className="text-2xl text-center overflow-hidden text-ellipsis whitespace-nowrap p-2 w-72">
-            {props.mangaFS.manga.title_ja === ""
-              ? props.mangaFS.manga.title
-              : props.mangaFS.manga.title +
+            {manga.title_ja === ""
+              ? manga.title
+              : manga.title +
                 " - " +
-                props.mangaFS.manga.title_ja}
+                manga.title_ja}
           </div>
           <div className="relative w-5/6 flex justify-center max-h-96">
-            {props.mangaFS.manga.img_large !== undefined ? (
+            {manga.img_large !== undefined ? (
               <Image
                 className="shadow-md shadow-white rounded-lg"
                 width={260}
                 height={0}
-                src={props.mangaFS.manga.img_large}
-                alt={props.mangaFS.manga.title + " manga cover"}
+                src={manga.img_large}
+                alt={manga.title + " manga cover"}
               />
             ) : (
               <img
                 className="p-6 invert h-full w-full"
                 src="/question-mark.svg"
-                alt={props.mangaFS.manga.title + " doesn't have manga cover"}
+                alt={manga.title + " doesn't have a manga cover"}
               />
             )}
           </div>
 
           <div className="p-2" />
           <Link
-            href={`/${encodeURIComponent(props.mangaFS.manga.mal_api_id)}/info`}
+            href={`/${encodeURIComponent(manga.mal_api_id)}/info`}
             onClick={() => {
               document.getElementById("toast")?.classList.add("hidden");
             }}
